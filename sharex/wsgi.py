a8fc9fd1671de5ffdd9324b80af8c068c12a97df
Martin Skow Røed <msroed@gmail.com>
2015-01-29 23:34:38 +0100

    Changed static root to fix static problem

--- sharex/wsgi.py
+++ sharex/wsgi.py
@@ -13,4 +13,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sharex.settings")
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+try:
+	import sharex.local_settings
+except:
+	application = DjangoWhiteNoise(application)
