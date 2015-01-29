9ee98882e062a46de2655585b15f3739b2d0332e
Martin Skow Røed <msroed@gmail.com>
2015-01-29 17:24:20 +0100

    Add localsettings to gitignore

--- sharex/local_settings.py
+++
@@ -1,8 +0,0 @@
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
