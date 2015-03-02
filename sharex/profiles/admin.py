3947c8043bc2959a31b72f7da89ba13a6fb30a0d
Martin Skow Røed <msroed@gmail.com>
2015-03-02 10:49:15 +0100

    Added startup to admin

--- sharex/profiles/admin.py
+++ sharex/profiles/admin.py
@@ -4,7 +4,11 @@ from django.contrib.auth.admin import UserAdmin
 
 @admin.register(Startup)
 class StartupAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name')
+	list_display = ('id', 'name', 'profiles')
+
+	def profiles(self, obj):
+		return ', '.join(profile.email for profile in obj.profiles.all())
+	profiles.short_description = 'Profiles'
 
 @admin.register(Profile)
 class ProfileAdmin(UserAdmin):
@@ -16,7 +20,7 @@ class ProfileAdmin(UserAdmin):
 	list_display = ('email', 'is_staff', 'is_superuser')
 	list_filter = ('is_superuser','is_staff')
 	fieldsets = (
-		(None, {'fields': ('email', 'password')}),
+		(None, {'fields': ('email', 'password', 'startup')}),
 	)
 	# add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
 	# overrides get_fieldsets to use this attribute when creating a user.
